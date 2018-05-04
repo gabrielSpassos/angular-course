b6228a733ece04f48d787ae01419c114b1e3051d
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-05-04 19:38:18 -0300

    :tada: criando typescript basic

---
+++ first-typescript/main.js
@@ -0,0 +1,12 @@
+var variable = 'My Variable';
+function myFunction(x, y) {
+    return x + y;
+}
+var number = 2;
+var PI = 3.14;
+var numbers = [1, 2, 3];
+numbers.map(function (value) {
+    return value * 2;
+});
+numbers.map(function (value) { return value * 2; });
+var n1 = 'dshjdhs';
---
+++ first-typescript/main.ts
@@ -0,0 +1,17 @@
+var variable = 'My Variable';
+
+function myFunction(x , y) {
+    return x + y;
+}
+
+let number = 2;
+const PI = 3.14; 
+
+var numbers = [1, 2, 3];
+numbers.map(function(value){
+    return value * 2;
+});
+
+numbers.map(value => value * 2);
+
+var n1: string = 'dshjdhs';
