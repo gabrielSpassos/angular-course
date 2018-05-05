b106213535b8c432db8597866547b4bb6c30def7
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-05-05 17:43:12 -0300

    :recycle: refatorando o data-binding component

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,10 @@ export class DataBindingComponent implements OnInit {
   url: string = 'https://github.com/gabrielSpassos';
   cursoAngular: boolean = true;
   imageUrl: string = 'http://ottawadogfestival.ca/wp-content/uploads/2016/06/Pierre.jpg';
+  value: string = '';
+  valorSalvo;
+  isMouseOver: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
@@ -21,6 +25,23 @@ export class DataBindingComponent implements OnInit {
 
   getJoinha() {
     return true;
+  }
+
+  onClick() {
+    console.log("Clicado");
+    alert("Click!")
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.value = (<HTMLInputElement>evento.target).value;
+  }
+
+  saveValue(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
 }
